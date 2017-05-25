d8b3236ae025a95791d3e3e2ac0fdffc7b4497ae
Application User <app@lxt-dev-apps-django01.in.linuxteam.at>
2017-05-25 14:24:23 +0000

    add sudo hint

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-test "`which sudo`" || apt-get install sudo
+test "`which sudo`" || ( echo "You need sudo installed and working"; exit 1; )
+
 ./tmux-config/setup_tmux.sh
 ./vim-config/setup_vim.sh
 ./zsh-config/setup_zsh.sh
