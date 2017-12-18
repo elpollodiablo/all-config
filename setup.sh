626985f0bd8afa2cff819a518a27e4eea68afa21
Philip Poten <philip@poten.at>
2017-12-18 10:01:01 +0100

    change to https urls, update submodules

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 test "`which sudo`" || ( echo "You need sudo installed and working"; exit 1; )
-
+git submodule init
+git submodule update
 ./tmux-config/setup_tmux.sh
 ./vim-config/setup_vim.sh
 ./zsh-config/setup_zsh.sh
