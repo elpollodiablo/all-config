b2252d6c4fca34afaef5877e47571d28a9ed1fa7
Philip Poten <philip@poten.at>
2018-06-02 17:06:53 +0200

    update submodule vim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 test "`which sudo`" || ( echo "You need sudo installed and working"; exit 1; )
 git submodule init
 git submodule update
+for dir in tmux-config vim-config zsh-config; do
+	pushd $dir;
+	git checkout master;
+	git pull;
+	popd;
+done
 ./tmux-config/setup_tmux.sh
 ./vim-config/setup_vim.sh
 ./zsh-config/setup_zsh.sh
